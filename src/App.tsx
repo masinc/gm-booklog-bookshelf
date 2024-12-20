b9789117f8a80d7d7f77f89d2c2b677bef2e4755
masinc <masinc000@gmail.com>
2024-12-20 19:57:08 +0900

    add: toogle bookshelf

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [usersHeader] = useState<HTMLElement>(document.getElementById("users-header") as HTMLElement);
+
+	useEffect(() => {
+		if (usersHeader) {
+			usersHeader.style.display = "none";
+		}
+	}, [usersHeader]);
+
+	function toggle() {
+		if (usersHeader) {
+			if (usersHeader.style.display === "none") {
+				usersHeader.style.display = "block";
+			} else {
+				usersHeader.style.display = "none";
+			}
+		}
+	}
 
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
+		<>
+			<button type="button" onClick={toggle} className="menu">本棚の表示</button>
+		</>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const headerMenu = document.getElementById("header-menu") as HTMLElement;
+
+if (!headerMenu) {
+	throw new Error("Element not found: #header-menu");
+}
+
 ReactDOM.createRoot(
 	(() => {
 		const app = document.createElement("div");
-		document.body.append(app);
+		headerMenu.appendChild(app);
 		return app;
 	})(),
 ).render(
