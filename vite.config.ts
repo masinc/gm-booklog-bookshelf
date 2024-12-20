5388c48ba6b936b1e1db9ab6d7ab69bc78f8901f
masinc <masinc000@gmail.com>
2024-12-20 19:32:45 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import monkey, { cdn } from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    monkey({
-      entry: "src/main.tsx",
-      userscript: {
-        icon: "https://www.google.com/s2/favicons?sz=64&domain=booklog.jp",
-        namespace: "https://masinc.dev",
-        match: ["https://booklog.jp/users/*"],
-        grant: "none",
-      },
-      build: {
-        externalGlobals: {
-          react: cdn.jsdelivr("React", "umd/react.production.min.js"),
-          "react-dom": cdn.jsdelivr(
-            "ReactDOM",
-            "umd/react-dom.production.min.js"
-          ),
-        },
-      },
-    }),
-  ],
+	plugins: [
+		react(),
+		monkey({
+			entry: "src/main.tsx",
+			userscript: {
+				icon: "https://www.google.com/s2/favicons?sz=64&domain=booklog.jp",
+				namespace: "https://masinc.dev",
+				match: ["https://booklog.jp/users/*"],
+				grant: "none",
+			},
+			build: {
+				externalGlobals: {
+					react: cdn.jsdelivr("React", "umd/react.production.min.js"),
+					"react-dom": cdn.jsdelivr(
+						"ReactDOM",
+						"umd/react-dom.production.min.js",
+					),
+				},
+			},
+		}),
+	],
 });
